b0f016ff1fd8e2fa383abd5216f26081b3bfc340
navadeep reddy <navadeep1904@gmail.com>
2025-09-25 21:31:30 +0530

    Update 2_detail.py

--- frontend_streamlit/streamlit_pages/pages/2_detail.py
+++ frontend_streamlit/streamlit_pages/pages/2_detail.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8003"
+import streamlit as st
+API_URL = st.secrets["API_URL"]
+API_KEY = st.secrets["API_KEY"]
+
 HEADERS = {
-    "X-API-KEY": "secret123",
+    "X-API-KEY": API_KEY,
     "Content-Type": "application/json"
 }
 
